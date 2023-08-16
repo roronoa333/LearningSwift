f3c90374107957cbd65eecae90052ca2cb7a2a39
Nripesh Pokala <nripeshd.ace@gmail.com>
2023-08-15 21:29:55 -0600

    Initial Commit

---
+++ MyFirstiOSApp/MyFirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstiOSAppApp.swift
+//  MyFirstiOSApp
+//
+//  Created by Nripesh Pokala on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
